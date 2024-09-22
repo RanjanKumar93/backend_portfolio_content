e1f1fd0353251df216d16c4dc02f4b749ab0e156
RanjanKumar93 <153518242+RanjanKumar93@users.noreply.github.com>
2024-09-22 17:26:59 +0530

    Update solidity_MyToken_ERC20.sol

--- solidity_MyToken_ERC20.sol
+++ solidity_MyToken_ERC20.sol
@@ -36,10 +36,12 @@ contract MyToken is IERC20 {
     string public name;
     string public symbol;
     bool public isPaused = false;
+    address[] public blacklist;
 
     mapping(address => uint256) public override balanceOf;
     mapping(address => mapping(address => uint256)) public allowances;
     mapping(address => bool) public isFrozen;
+    mapping(address => bool) private addressExistsInBlacklist;
 
     event Minted(address indexed account, uint256 amount);
     event Burned(address indexed account, uint256 amount);
@@ -47,6 +49,8 @@ contract MyToken is IERC20 {
     event Unpaused();
     event FrozenAccount(address indexed account);
     event UnfrozenAccount(address indexed account);
+    event blacklistAccount(address indexed account);
+    event unBlacklistAccount(address indexed account);
 
     uint256 private constant NOT_ENTERED = 1;
     uint256 private constant ENTERED = 2;
@@ -61,7 +65,7 @@ contract MyToken is IERC20 {
 
     constructor() {
         contractOwner = msg.sender;
-        totalSupply = 1_000_000 * 10**18; // 1 million tokens with 18 decimals
+        totalSupply = 1_000_000 * 10**18;
         decimals = 18;
         name = "MyToken";
         symbol = "MTK";
@@ -83,8 +87,11 @@ contract MyToken is IERC20 {
         _;
     }
 
-    modifier whenAccountNotFrozen(address account) {
-        require(!isFrozen[account], "Account is frozen");
+    modifier whenAccountNotStopped(address account) {
+        require(
+            !isFrozen[account] && !addressExistsInBlacklist[account],
+            "Account is stopped"
+        );
         _;
     }
 
@@ -92,8 +99,8 @@ contract MyToken is IERC20 {
         external
         override
         whenNotPaused
-        whenAccountNotFrozen(msg.sender)
-        whenAccountNotFrozen(to)
+        whenAccountNotStopped(msg.sender)
+        whenAccountNotStopped(to)
         nonReentrant
         returns (bool)
     {
@@ -121,8 +128,8 @@ contract MyToken is IERC20 {
         public
         override
         whenNotPaused
-        whenAccountNotFrozen(msg.sender)
-        whenAccountNotFrozen(spender)
+        whenAccountNotStopped(msg.sender)
+        whenAccountNotStopped(spender)
         returns (bool success)
     {
         require(spender != address(0), "Invalid spender address");
@@ -141,9 +148,9 @@ contract MyToken is IERC20 {
         external
         override
         whenNotPaused
-        whenAccountNotFrozen(from)
-        whenAccountNotFrozen(msg.sender)
-        whenAccountNotFrozen(to)
+        whenAccountNotStopped(from)
+        whenAccountNotStopped(msg.sender)
+        whenAccountNotStopped(to)
         nonReentrant
         returns (bool)
     {
@@ -167,7 +174,7 @@ contract MyToken is IERC20 {
         external
         onlyOwner
         whenNotPaused
-        whenAccountNotFrozen(account)
+        whenAccountNotStopped(account)
     {
         require(account != address(0), "Invalid account address");
         require(value > 0, "Value must be greater than zero");
@@ -181,7 +188,7 @@ contract MyToken is IERC20 {
     function burn(uint256 amount)
         external
         whenNotPaused
-        whenAccountNotFrozen(msg.sender)
+        whenAccountNotStopped(msg.sender)
     {
         require(amount > 0, "Amount must be greater than zero");
         require(
@@ -227,179 +234,28 @@ contract MyToken is IERC20 {
         require(newOwner != address(0), "New owner is the zero address");
         contractOwner = newOwner;
     }
+
+    function addToBlacklist(address account) external onlyOwner {
+        require(account != address(0), "Invalid account address");
+        require(!addressExistsInBlacklist[account], "Address already exists");
+        blacklist.push(account);
+        addressExistsInBlacklist[account] = true;
+        emit blacklistAccount(account);
+    }
+
+    function unblacklist(address account) external onlyOwner {
+        require(account != address(0), "Invalid account address");
+        require(addressExistsInBlacklist[account], "Address not in blacklist");
+        uint256 length = blacklist.length;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (blacklist[i] == account) {
+                blacklist[i] = blacklist[length - 1];
+                blacklist.pop();
+                addressExistsInBlacklist[account] = false;
+                emit unBlacklistAccount(account);
+                return;
+            }
+        }
+    }
 }
-
-// pragma solidity ^0.8.24;
-
-// interface IERC20 {
-//     event Transfer(address indexed from, address indexed to, uint256 value);
-//     event Approval(
-//         address indexed owner,
-//         address indexed spender,
-//         uint256 value
-//     );
-
-//     function totalSupply() external view returns (uint256);
-
-//     function balanceOf(address account) external view returns (uint256);
-
-//     function transfer(address to, uint256 value) external returns (bool);
-
-//     function allowance(address owner, address spender)
-//         external
-//         view
-//         returns (uint256);
-
-//     function approve(
-//         address _spender,
-//         uint256 _currentValue,
-//         uint256 _newValue
-//     ) external returns (bool);
-
-//     function transferFrom(
-//         address from,
-//         address to,
-//         uint256 value
-//     ) external returns (bool);
-// }
-
-// contract MyToken is IERC20 {
-//     address public contractOwner;
-//     uint256 public override totalSupply;
-//     uint8 public decimals;
-//     string public name;
-//     string public symbol;
-//     bool public isPause = false;
-
-//     mapping(address => uint256) public override balanceOf;
-//     mapping(address => mapping(address => uint256)) public allowances;
-//     mapping(address => bool) public freezedAcc;
-
-//     constructor() {
-//         contractOwner = msg.sender;
-//         totalSupply = 1000000;
-//         decimals = 18;
-//         name = "MyToken";
-//         symbol = "MTK";
-//         balanceOf[contractOwner] = totalSupply;
-//     }
-
-//     event Minted(address indexed account, uint256 amount);
-//     event Burned(address indexed account, uint256 amount);
-//     event FreezedAccount(address indexed account);
-//     event UnfreezeAccount(address indexed account);
-
-//     modifier pauseCheck() {
-//         require(!isPause, "p");
-//         _;
-//     }
-
-//     function transfer(address to, uint256 value)
-//         external
-//         override
-//         pauseCheck
-//         returns (bool)
-//     {
-//         require(to != address(0), "ERC20: transfer to the zero address");
-//         require(value > 0, "ERC20: value must be greater than zero");
-//         require(freezedAcc[msg.sender], "freezed by owner");
-//         require(freezedAcc[to], "freezed by owner");
-//         require(balanceOf[msg.sender] >= value, "ERC20: insufficient balance");
-
-//         balanceOf[msg.sender] -= value;
-//         balanceOf[to] += value;
-
-//         emit Transfer(msg.sender, to, value);
-//         return true;
-//     }
-
-//     function allowance(address owner, address spender)
-//         external
-//         view
-//         override
-//         pauseCheck
-//         returns (uint256)
-//     {
-//         return allowances[owner][spender];
-//     }
-
-//     function approve(
-//         address _spender,
-//         uint256 _currentValue,
-//         uint256 _newValue
-//     ) public override pauseCheck returns (bool success) {
-//         require(freezedAcc[msg.sender], "freezed by owner");
-//         require(freezedAcc[_spender], "freezed by owner");
-//         if (allowances[msg.sender][_spender] == _currentValue) {
-//             allowances[msg.sender][_spender] = _newValue;
-//             emit Approval(msg.sender, _spender, _newValue);
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
-
-//     function transferFrom(
-//         address from,
-//         address to,
-//         uint256 value
-//     ) external override pauseCheck returns (bool) {
-//         require(freezedAcc[msg.sender], "freezed by owner");
-//         require(freezedAcc[from], "freezed by owner");
-//         require(freezedAcc[to], "freezed by owner");
-//         require(to != address(0), "ERC20: transfer to the zero address");
-//         require(value > 0, "ERC20: value must be greater than zero");
-//         require(
-//             allowances[from][msg.sender] >= value,
-//             "ERC20: transfer amount exceeds allowance"
-//         );
-//         require(balanceOf[from] >= value, "ERC20: insufficient balance");
-
-//         allowances[from][msg.sender] -= value;
-//         balanceOf[from] -= value;
-//         balanceOf[to] += value;
-
-//         emit Transfer(from, to, value);
-//         return true;
-//     }
-
-//     function mint(address account, uint256 value) external pauseCheck {
-//         require(msg.sender == contractOwner, "not allowed");
-//         require(freezedAcc[account], "freezed account");
-//         require(account != address(0), "ad0");
-
-//         totalSupply += value;
-//         balanceOf[account] += value;
-//         emit Minted(account, value);
-//     }
-
-//     function burn(uint256 amount) external pauseCheck {
-//         require(amount > 0, "oooo");
-//         require(balanceOf[msg.sender] >= amount, "ooe");
-
-//         balanceOf[msg.sender] -= amount;
-//         totalSupply -= amount;
-//         emit Burned(msg.sender, amount);
-//     }
-
-//     function freezeAccount(address account) external pauseCheck {
-//         require(msg.sender == contractOwner, "ab");
-//         freezedAcc[account] = false;
-//         emit FreezedAccount(account);
-//     }
-
-//     function unfreezeAccount(address account) external pauseCheck {
-//         require(msg.sender == contractOwner, "ab");
-//         freezedAcc[account] = true;
-//         emit UnfreezeAccount(account);
-//     }
-
-//     function pause() external {
-//         require(msg.sender == contractOwner);
-//         isPause = true;
-//     }
-//     function unpause()external {
-//         require(msg.sender == contractOwner);
-//         isPause = false;
-//     }
-// }
